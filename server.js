7be2342c772490cfa7c99ade874efeda54f18355
@Roninanu777 <ronirajkamalpradhan1112@gmail.com>
2020-12-14 04:17:01 +0530

    server initialization

--- server.js
+++ server.js
@@ -1,1 +1,19 @@
 require("dotenv").config({ path: "./.env" });
+const express = require("express");
+const cors = require("cors");
+const helmet = require("helmet");
+
+//initialize express
+const app = express();
+
+//initialize middlewares
+app.use(cors());
+app.use(helmet());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server is up and running on PORT ${PORT}`);
+});
